ae80900600abac19a7019756f448deb94027e743
Sebastian Baeza <seba.baeza@gmail.com>
2025-07-11 09:52:24 +1000

    dim tables, business questions and fact table scripts

--- scripts/bronze_table_creation.sql
+++ scripts/bronze_table_creation.sql
@@ -7,6 +7,27 @@ passenger count, trip distance, rate code, location IDs, payment type, fare amou
 and additional surcharges.
 */
 
+
+/* 
+LOADING DATA FROM PARQUET
+
+COPY INTO "NYC_YELLOW"."TRIPS_BRONZE"."YELLOW_TRIPDATA"
+FROM (
+    SELECT $1:VendorID::NUMBER(38, 0), $1:tpep_pickup_datetime::NUMBER(38, 0), $1:tpep_dropoff_datetime::NUMBER(38, 0), $1:passenger_count::NUMBER(38, 0), $1:trip_distance::FLOAT, $1:RatecodeID::NUMBER(38, 0), $1:store_and_fwd_flag::VARCHAR, $1:PULocationID::NUMBER(38, 0), $1:DOLocationID::NUMBER(38, 0), $1:payment_type::NUMBER(38, 0), $1:fare_amount::FLOAT, $1:extra::FLOAT, $1:mta_tax::FLOAT, $1:tip_amount::FLOAT, $1:tolls_amount::FLOAT, $1:improvement_surcharge::FLOAT, $1:total_amount::FLOAT, $1:congestion_surcharge::FLOAT, $1:Airport_fee::FLOAT, $1:cbd_congestion_fee::FLOAT
+    FROM '@"NYC_YELLOW"."TRIPS_BRONZE"."RAW"'
+)
+FILES = ('yellow_tripdata_2025-03.parquet')
+FILE_FORMAT = (
+    TYPE=PARQUET,
+    REPLACE_INVALID_CHARACTERS=TRUE,
+    BINARY_AS_TEXT=FALSE
+)
+ON_ERROR=ABORT_STATEMENT;
+
+*/
+
+
+
 use database NYC_YELLOW; 
 create TABLE IF NOT EXISTS YELLOW_TRIPDATA (
 	VENDORID NUMBER(38,0),
@@ -30,7 +51,6 @@ create TABLE IF NOT EXISTS YELLOW_TRIPDATA (
 	AIRPORT_FEE FLOAT,
 	CBD_CONGESTION_FEE FLOAT
 );
-
 
 
 /* 
